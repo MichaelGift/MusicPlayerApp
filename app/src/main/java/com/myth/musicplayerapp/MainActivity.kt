257337eb481ac50c6e1e867e94e629a605e6c303
MichaelGift <simiyugift@outlook.com>
2023-04-16 03:34:05 +0300

    V 1.0.6 Refactored to use data binding

--- app/src/main/java/com/myth/musicplayerapp/MainActivity.kt
+++ app/src/main/java/com/myth/musicplayerapp/MainActivity.kt
@@ -1,41 +1,44 @@
-
 package com.myth.musicplayerapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
+import androidx.databinding.DataBindingUtil
 import com.google.android.material.tabs.TabLayoutMediator
 import com.myth.musicplayerapp.adapter.MusicPlayerAdapter
+import com.myth.musicplayerapp.databinding.ActivityMainBinding
 import com.myth.musicplayerapp.models.TabIconData
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val tabLayout : TabLayout = findViewById(R.id.tabViewNav)
-        val viewPager : ViewPager2 = findViewById(R.id.viewPager)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val musicViewAdapter = MusicPlayerAdapter(
             supportFragmentManager,
             lifecycle
         )
 
-        val iconSet = generateViewIcon()
-        viewPager.adapter = musicViewAdapter
-        TabLayoutMediator(tabLayout, viewPager){
-            tab,position ->
-            tab.icon = ContextCompat.getDrawable(this, iconSet[position].activeTabIcon)
-        }.attach()
+        binding.apply {
 
-        tabLayout.getTabAt(1)?.select()
+            viewPager.adapter = musicViewAdapter
+            val iconSet = generateViewIcon()
+
+            TabLayoutMediator(tabViewNav, viewPager) { tab, position ->
+                tab.icon =
+                    ContextCompat.getDrawable(this@MainActivity, iconSet[position].activeTabIcon)
+            }.attach()
+
+            tabViewNav.getTabAt(1)?.select()
+        }
     }
 
-    private fun generateViewIcon():ArrayList<TabIconData>{
+    private fun generateViewIcon(): ArrayList<TabIconData> {
         val icons = ArrayList<TabIconData>()
 
         val playMusic = TabIconData(R.drawable.musicplayicon)
--- app/src/main/java/com/myth/musicplayerapp/fragments/PlayerFragment.kt
+++ app/src/main/java/com/myth/musicplayerapp/fragments/PlayerFragment.kt
@@ -4,95 +4,94 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.SeekBar
-import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import com.myth.musicplayerapp.R
+import com.myth.musicplayerapp.databinding.FragmentPlayerBinding
 import java.util.concurrent.TimeUnit
 
 @Suppress("DEPRECATION")
 class PlayerFragment : Fragment() {
 
-    var isPlaying : Boolean = false
+    var isPlaying: Boolean = false
+
     private var startTime = 0.0
     private var finalTime = 0.0
     private var forwardTime = 10000
     private var rewindTime = 10000
     private var oneTimeOnly = 0
 
-    private var handler : Handler = Handler()
-    private lateinit var mediaPlayer : MediaPlayer
-    private lateinit var timeText : TextView
-    private lateinit var songLength : TextView
-    private lateinit var seekBar: SeekBar
-    lateinit var playButton: Button
-    lateinit var pauseButton: Button
+    private var handler: Handler = Handler()
+    private lateinit var mediaPlayer: MediaPlayer
+
+    private lateinit var binding: FragmentPlayerBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view  = inflater.inflate(R.layout.fragment_player, container, false)
+    ): View {
 
-        pauseButton = view.findViewById(R.id.pause_button)
-        pauseButton.setOnClickListener{
-            pauseMusic(it)
-        }
-
-        playButton= view.findViewById(R.id.play_button)
-        playButton.setOnClickListener{
-            playMusic(it)
-        }
-
-        val rewindButton : Button = view.findViewById(R.id.rewind_button)
-        rewindButton.setOnClickListener{
-            rewindMusic(it)
-        }
-
-        val forwardButton: Button = view.findViewById(R.id.forward_button)
-        forwardButton.setOnClickListener{
-            forwardMusic(it)
-        }
-
-
-        val titleText = view.findViewById<TextView>(R.id.song_title)
-        timeText = view.findViewById(R.id.time_left_text)
-        songLength = view.findViewById(R.id.song_length)
-        seekBar = view.findViewById(R.id.seek_bar)
-
-        mediaPlayer = MediaPlayer.create(
-            view.context,
-            R.raw.take_over
+        binding = FragmentPlayerBinding.inflate(
+            inflater,
+            container,
+            false
         )
 
-        pauseButton.isVisible = false
-        seekBar.isClickable = false
+        binding.apply {
 
-        titleText.text= resources.getResourceEntryName(R.raw.take_over)
+            playButton.setOnClickListener {
+                playMusic()
+            }
+
+            pauseButton.setOnClickListener {
+                pauseMusic()
+            }
+
+            rewindButton.setOnClickListener {
+                rewindMusic()
+            }
+
+            forwardButton.setOnClickListener {
+                forwardMusic()
+            }
+
+            pauseButton.isVisible = false
+            seekBar.isClickable = false
+
+            mediaPlayer = MediaPlayer.create(
+                context,
+                R.raw.take_over
+            )
+
+            textView.text = resources.getResourceEntryName(R.raw.take_over)
+        }
 
         handler.postDelayed(updateSongTime, 100)
 
-        return view
+        return binding.root
     }
-    private fun playMusic(view: View){
-        view as Button
+
+
+    private fun playMusic() {
+
         mediaPlayer.start()
+
         finalTime = mediaPlayer.duration.toDouble()
         startTime = mediaPlayer.currentPosition.toDouble()
 
-        if(oneTimeOnly == 0){
-            seekBar.max = finalTime.toInt()
+        if (oneTimeOnly == 0) {
+
+            binding.seekBar.max = finalTime.toInt()
             oneTimeOnly = 1
         }
 
-        timeText.text = startTime.toString()
-        songLength.text = buildString {
+        binding.timeLeftText.text = startTime.toString()
+
+        binding.songLength.text = buildString {
             append(
                 String.format(
                     "%d:%02d",
@@ -101,48 +100,70 @@ class PlayerFragment : Fragment() {
                 )
             )
         }
-        seekBar.progress = startTime.toInt()
+
+        binding.seekBar.progress = startTime.toInt()
 
         handler.postDelayed(updateSongTime, 100)
+
         isPlaying = true
     }
-    private fun pauseMusic(view: View){
-        view as Button
+
+    private fun pauseMusic() {
+
         mediaPlayer.pause()
+
         isPlaying = false
     }
-    private fun forwardMusic(view: View){
-        view as Button
+
+    private fun forwardMusic() {
+
         val temp = startTime
-        if((temp + forwardTime)<= finalTime){
+
+        if ((temp + forwardTime) <= finalTime) {
+
             startTime += forwardTime
             mediaPlayer.seekTo(startTime.toInt())
-        }else{
+
+        } else {
+
             Toast.makeText(
-                view.context,
+                context,
                 "Cannot Jump Further than Song Length",
-                Toast.LENGTH_LONG).show()
-        }
-    }
-    private fun rewindMusic(view : View){
-        view as Button
-        val temp = startTime.toInt()
-        if((temp - rewindTime)>0){
-            startTime -= rewindTime
-            mediaPlayer.seekTo(startTime.toInt())
-        }else{
-            Toast.makeText(
-                view.context,
-                "Cannot Jump to Before Song Started",
-                Toast.LENGTH_LONG).show()
+                Toast.LENGTH_LONG
+            ).show()
+
         }
     }
 
-    private val updateSongTime : Runnable = object : Runnable {
+    private fun rewindMusic() {
+
+
+        val temp = startTime.toInt()
+
+        if ((temp - rewindTime) > 0) {
+
+            startTime -= rewindTime
+            mediaPlayer.seekTo(startTime.toInt())
+
+        } else {
+
+            Toast.makeText(
+                context,
+                "Cannot Jump to Before Song Started",
+                Toast.LENGTH_LONG
+            ).show()
+
+        }
+    }
+
+    private val updateSongTime: Runnable = object : Runnable {
+
         override fun run() {
+
             startTime = mediaPlayer.currentPosition.toDouble()
+
             try {
-                timeText.text = buildString {
+                binding.timeLeftText.text = buildString {
                     append(
                         String.format(
                             "%d:%02d",
@@ -151,24 +172,35 @@ class PlayerFragment : Fragment() {
                         )
                     )
                 }
+
             } catch (e: Exception) {
+
                 Log.e("MainActivity", "Error updating timer text: ${e.message}")
             }
-            seekBar.progress = startTime.toInt()
-            if(!isPlaying){
-                playButton.isVisible = true
-                pauseButton.isVisible = false
+
+            binding.apply {
+
+                seekBar.progress = startTime.toInt()
+
+                if (!isPlaying) {
+
+                    playButton.isVisible = true
+                    pauseButton.isVisible = false
+                } else {
+
+                    playButton.isVisible = false
+                    pauseButton.isVisible = true
+                }
             }
-            else{
-                playButton.isVisible = false
-                pauseButton.isVisible = true
-            }
+
             handler.postDelayed(this, 100)
         }
     }
 
     override fun onDestroy() {
+
         super.onDestroy()
+
         mediaPlayer.release()
         handler.removeCallbacks(updateSongTime)
     }
